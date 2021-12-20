569ada397698485db3333423236d75dae21d1a6f
dperezc21 <84674334+dperezc21@users.noreply.github.com>
2021-12-20 09:50:14 -0500

    Create updateStatusFileService.ts

---
+++ src/services/fileService/updateStatusFileService.ts
@@ -0,0 +1,25 @@
+import File from "../../models/modelFile";
+
+const updateStatusFiles = async(dataFile:any) =>{
+    const {data} = dataFile;
+
+    try {
+
+        let file_names:any = [];
+        for (const iterator of data) {
+            console.log(iterator.file_name)
+            const file = await File.findOneAndUpdate({file_name:iterator.file_name}, {file_status:iterator.file_status}, {new:true});
+            if (file) file_names.push(file.file_name);
+        }
+        return {
+            status:200,
+            message:file_names
+        }
+    } catch (error) {
+        return {
+            status:500,
+            message:error
+        }
+    }
+
+}
