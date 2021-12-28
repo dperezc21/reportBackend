ed6d47971c666434b68058328e8b8293b07308f8
dperezc21 <davierperez11.2@gmail.com>
2021-12-28 09:07:27 -0500

    archivos movidos a userService

---
+++ src/services/userServices/getCompanyUserService.ts
@@ -0,0 +1,31 @@
+const {getAuthUser} = require( "../../middleware/verifyToken");
+const User = require('../../models/modelUser')
+
+
+const getCompanyUserRepository = async(query:any) =>{
+
+    const {com_id, _id } = getAuthUser();
+    const {user_id} = query;
+    console.log( com_id);
+    
+    try {
+        let users:any;
+        if(!user_id){
+            users = await User.find({com_id, _id:{$ne:_id},user_status:true}).populate('pro_code');
+        }else{
+            users = await User.findOne({com_id, _id:{$eq:user_id, $ne:_id}, user_status:true}).populate('pro_code')
+        }
+        if (users){
+            return {status:200, message:users}
+        }
+        
+        return {status:602, message:"usuario de compañia no encontrado"}
+    } catch (error:any) {
+        console.log(error)
+        return {
+            status:500,
+            message:error}
+    }
+}
+
+export = getCompanyUserRepository;
---
+++ src/services/userServices/updateCompanyUserService.ts
@@ -0,0 +1,32 @@
+
+const {getDataUserValid} = require( "../../helpers/helperUser");
+
+const User = require('../../models/modelUser')
+
+const updateCompanyUserRepository = async(dataUser:any) => {
+    let {user_id, data} = dataUser;
+    try {
+        data = await getDataUserValid(data);
+        console.log("data company", dataUser);
+        const updateUser = await User.findOneAndUpdate({_id:user_id, user_status:true}, {$set:data}, {new:true});
+        if (!updateUser){
+            return {
+                status:602,
+                message:"usuario no actualizado, no existe"
+            };
+        }
+        return {
+            status:200,
+            message:"usuario actualizado"
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            status:500,
+            message:error
+        }
+    }
+}
+
+
+export = updateCompanyUserRepository;
