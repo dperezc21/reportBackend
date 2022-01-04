2b3cde27645a6019def34c0372192f1444675bb7
dperezc21 <davierperez11.2@gmail.com>
2022-01-04 14:33:52 -0500

    metodo para obtener el codigo de la compañia

---
+++ src/services/companyServices/getCompanyCodeService.ts
@@ -0,0 +1,25 @@
+import CompanyInterface from "../../interfaces/companyInterface";
+import modelCompany from "../../models/modelCompany";
+const {getAuthUser} = require( "../../middleware/verifyToken");
+
+
+const getCompanyCodeRepository = async() => {
+    const {com_id} = getAuthUser();
+
+    try {
+        const company: CompanyInterface = await modelCompany.findOne({_id:com_id, com_status:true});
+        const code: string | Error = company?.com_code;
+
+        return {
+            status:200,
+            code
+        }
+    } catch (error:any) {
+        return {
+            status:500,
+            message: error
+        }
+    }
+}
+
+export = getCompanyCodeRepository;
