977fa14ecc5eb0a53a189bb3e42609f0f59e51be
dperezc21 <davierperez11.2@gmail.com>
2022-01-17 15:48:25 -0500

    configuracion de archivos y reportes

--- src/helpers/dataConfig.ts
+++ src/helpers/dataConfig.ts
@@ -4,26 +4,24 @@ const configFile: any = {
     image_format: 'imeage/jpeg',
     video_format:'video/mp4',
     audio_format: 'audio/mp4',
-    number_images_allowed: 3,
-    number_videos_allowed: 1,
-    number_audios_allowed: 1,
+    number_images: 3,
+    number_videos: 1,
+    number_audios: 1,
 }
 
 const configUser: object = {
-    pro_name: "user"
+    pro_name: "user",
+    number_users: 5
 }
 
 const configCompany:object = {
     pro_name: "admin",
-    num_max_users: 5,
-    number_max_reports: 100,
-    num: configFile.number_audios_allowed + 
-         configFile.number_videos_allowed + 
-         configFile.number_images_allowed
 }
 
 const configReport: object = {
-    max_days_to_display_reports: 90
+    number_reports: 100,
+    max_days_to_display_reports: 90,
+    number_files_repots: configFile.number_images + configFile.number_videos + configFile.number_audios
 }
 
 
