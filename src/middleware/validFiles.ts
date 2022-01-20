00179b32e6540b4614f063282b5a9d23ad799e17
dperezc21 <davierperez11.2@gmail.com>
2022-01-20 14:41:13 -0500

    validanso numero de archivos

--- src/middleware/validFiles.ts
+++ src/middleware/validFiles.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import FileInterface from "../interfaces/fileInterface";
+import modelFile from "../models/modelFile";
 const { configFile } = require("../helpers/dataConfig");
 const { numberFiles } = require('../helpers/helperFile')
 
@@ -8,7 +10,14 @@ class ValidFiles {
     validNumberImages = async (req: Request, res: Response, next: any) => {
         const { files, rep_code } = req.body;
         try {
-            let numImages = await numberFiles(files, rep_code, configFile.image_format);
+            //let numImages = await numberFiles(files, rep_code, configFile.image_format);
+            let numImages = await modelFile.find({ rep_code, type_file:configFile.image_format}).count();
+            files.forEach((file: FileInterface) => {
+                if (file.file_type == configFile.image_format) {
+                    numImages += 1;
+                    console.log(file.file_type)
+                }
+            });
             if (numImages > configFile.number_images) {
                 return res.json({
                     status: 400,
@@ -30,7 +39,13 @@ class ValidFiles {
     validNumberVideos = async (req: Request, res: Response, next: any) => {
         const { files, rep_code } = req.body;
         try {
-            const numVideos = await numberFiles(files, rep_code, configFile.video_format);
+            let numVideos = await modelFile.find({ rep_code, type_file:configFile.video_format}).count();
+            files.forEach((file: FileInterface) => {
+                if (file.file_type == configFile.video_format) {
+                    numVideos += 1;
+                    console.log(file.file_type)
+                }
+            });
             if (numVideos > configFile.number_videos) {
                 return res.json({
                     status: 400,
@@ -51,7 +66,13 @@ class ValidFiles {
     validNumberAudios = async (req: Request, res: Response, next: any) => {
         const { files, rep_code } = req.body;
         try {
-            const numAudios = await numberFiles(files, rep_code, configFile.audio_format)
+            let numAudios = await modelFile.find({ rep_code, type_file:configFile.audio_format}).count();
+            files.forEach((file: FileInterface) => {
+                if (file.file_type == configFile.audio_format) {
+                    numAudios += 1;
+                    console.log(file.file_type)
+                }
+            });
             if (numAudios > configFile.number_audios) {
                 return res.json({
                     status: 400,
