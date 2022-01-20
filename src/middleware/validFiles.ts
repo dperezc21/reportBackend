dbe46892b76abbef21e91da491aa146f89b2ef3e
dperezc21 <davierperez11.2@gmail.com>
2022-01-20 15:05:36 -0500

    numero de archivos

--- src/middleware/validFiles.ts
+++ src/middleware/validFiles.ts
@@ -11,11 +11,13 @@ class ValidFiles {
         const { files, rep_code } = req.body;
         try {
             //let numImages = await numberFiles(files, rep_code, configFile.image_format);
-            let numImages = await modelFile.find({ rep_code, type_file:configFile.image_format}).count();
+            let images = await modelFile.find({ rep_code, type_file:configFile.image_format});
+            let numImages = images.length
+            console.log("numero imagenes", numImages)
             files.forEach((file: FileInterface) => {
                 if (file.file_type == configFile.image_format) {
                     numImages += 1;
-                    console.log(file.file_type)
+                    console.log("tipo de archivo",file.file_type)
                 }
             });
             if (numImages > configFile.number_images) {
@@ -39,17 +41,19 @@ class ValidFiles {
     validNumberVideos = async (req: Request, res: Response, next: any) => {
         const { files, rep_code } = req.body;
         try {
-            let numVideos = await modelFile.find({ rep_code, type_file:configFile.video_format}).count();
+            let videos:FileInterface[] = await modelFile.find({ rep_code, type_file:configFile.video_format})
+            let numVideos:number = videos.length;
+            console.log("numero de videos", numVideos)
             files.forEach((file: FileInterface) => {
                 if (file.file_type == configFile.video_format) {
                     numVideos += 1;
-                    console.log(file.file_type)
+                    console.log("tipo de archivo",file.file_type)
                 }
             });
             if (numVideos > configFile.number_videos) {
                 return res.json({
                     status: 400,
-                    message: "numero de videos exedidios"
+                    message: "numero de videos excedidos"
                 })
             }
             next();
@@ -66,11 +70,13 @@ class ValidFiles {
     validNumberAudios = async (req: Request, res: Response, next: any) => {
         const { files, rep_code } = req.body;
         try {
-            let numAudios = await modelFile.find({ rep_code, type_file:configFile.audio_format}).count();
+            let audios:FileInterface[] = await modelFile.find({ rep_code, type_file:configFile.audio_format});
+            let numAudios:number = audios.length
+            console.log("numero de auidos", numAudios)
             files.forEach((file: FileInterface) => {
                 if (file.file_type == configFile.audio_format) {
                     numAudios += 1;
-                    console.log(file.file_type)
+                    console.log("tipo de archivo",file.file_type)
                 }
             });
             if (numAudios > configFile.number_audios) {
