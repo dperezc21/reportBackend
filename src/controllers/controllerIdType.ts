1da3f65ba9f89c19f04120557cea871d960976e5
dperezc21 <davierperez11.2@gmail.com>
2022-01-26 16:59:20 -0500

    tipos identificacion de usuario

---
+++ src/controllers/controllerIdType.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+const getIdTypeService = require('../services/IdTypeService/getIdTypesServices')
+const insertIdTypeService = require('../services/idTypeService/insertIdTypeService')
+
+class ControllerIdType {
+
+    getIdTypes = async(req:Request, res:Response, next:any) =>{
+
+        try {
+            const response: object = await getIdTypeService();
+            return res.json(response)
+        } catch (error:any) {
+            return res.json({
+                status:500,
+                message:error.message
+            })
+        }
+
+    }
+
+    insertIdTypes = async(req:Request, res:Response, next:any) =>{
+        const paramtro:any = req.params.idType
+        try {
+            const response: object = await insertIdTypeService(paramtro);
+            return res.json(response)
+        } catch (error:any) {
+            return res.json({
+                status:500,
+                message:error.message
+            })
+        }
+
+    }
+}
+
+export = new ControllerIdType
